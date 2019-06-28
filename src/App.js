217ee2877780cef21b3c176a522f91381b8b2af4
Yihua Zhang <michaelzhang2@msn.com>
2019-06-28 08:58:21 -0400

    adding example for useMemo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Person from './person.component';
+const App = () => {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-class App extends React.Component {
-  constructor() {
-    super();
+  const incrementCount1 = useCallback(() => setCount1(count1 + 1), [count1]);
+  const incrementCount2 = useCallback(() => setCount2(count2 + 1), [count2]);
 
-    this.state = {
-      count: 0,
-      person: { name: 'Jack', age: 22 },
-      showPerson: false
-    };
-  }
+  const doSomethingComplicated = useMemo(() => {
+    console.log('I am computing something complex');
+    return ((count1 * 1000) % 12.4) * 51000 - 4000;
+  }, [count1]);
 
-  render() {
-    const { count, person, showPerson } = this.state;
-
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          {showPerson ? <Person person={person} /> : null}
-          Button Count: {count}
-          <button onClick={() => this.setState({ count: count + 1 })}>
-            Increase Count
-          </button>
-          <button onClick={() => this.setState({ showPerson: !showPerson })}>
-            Toggle Person
-          </button>
-        </header>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        Count1: {count1}
+        <button onClick={incrementCount1}>Increase Count1</button>
+        Count2: {count2}
+        <button onClick={incrementCount2}>Increase Count2</button>
+        complexValue: {doSomethingComplicated}
+      </header>
+    </div>
+  );
+};
 
 export default App;
